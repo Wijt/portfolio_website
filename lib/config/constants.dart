753679b6621bdff3ca5507efb052078187838e2a
Furkan Kaya <fkaya.personal@gmail.com>
2025-12-11 20:59:12 +0300

    feat: Add social media profile links and language-specific text constants.

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -6,7 +6,7 @@ class Constants {
   static const PROFILE_FACEBOOK = 'https://www.facebook.com/8fkaya/';
   static const PROFILE_GITHUB = 'https://github.com/wijt';
   static const PROFILE_TWITTER = 'https://twitter.com/iamfurkannn';
-  static const PROFILE_INSTAGRAM = 'https://www.instagram.com/iamfurkank_/';
+  static const PROFILE_INSTAGRAM = 'https://www.instagram.com/frkn.ky_/';
   static Language tr = Language(
     flag: SvgPicture.asset("assets/flag/tr.svg"),
     title: "Hoş geldiniz.",
